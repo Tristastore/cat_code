17df6ca1e095e5abd3cf1e1851091e1a1ee1241b
Kownzird <53167941+Kownzird@users.noreply.github.com>
2019-07-25 09:49:29 +0800

    Add files via upload

---
+++ cat.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	FILE *src_fp;
+	int read_ret;
+	
+	if(argc<2)
+	{
+		printf("please input src file\n");
+		return -1;
+	}
+	
+	src_fp=fopen(argv[1],"r");
+	if(src_fp==NULL)
+	{
+		printf("open src_file %s failure\n",argv[1]);
+		return -2;
+	}
+	//printf("open src_file %s success\n",argv[1]);
+	
+	while(1)
+	{
+		read_ret=fgetc(src_fp);
+		if(feof(src_fp))
+		{
+			//printf("read file %s end\n",argv[1]);
+			break;
+		}
+		fputc(read_ret,stdout);
+	}
+	fclose(src_fp);
+	return 0;
+}
